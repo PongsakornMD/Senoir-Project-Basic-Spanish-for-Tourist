0952130939a984e151fbc2af218ea48466f7a6d4
MDBonito <MAydaYBonito@gmail.com>
2016-11-30 19:32:15 +0700

    Create MainMenu page ^^

--- app/src/main/java/com/example/maydaybonito/basicspanishfortourist/MainMenuActivity.java
+++ app/src/main/java/com/example/maydaybonito/basicspanishfortourist/MainMenuActivity.java
@@ -5,16 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class MainMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
-
-    public void click2MainMenuPage (View view){
-        Intent i = new Intent(this, MainMenuActivity.class);
-        startActivity(i);
+        setContentView(R.layout.activity_main_menu);
     }
 }
